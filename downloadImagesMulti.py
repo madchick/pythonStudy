4e20a2dd1d289bcd35a2c6f5478476661bff6408
madchick <madchick@gmail.com>
2020-09-09 12:24:01 +0900

    Fix urlopen exception handling

--- downloadImagesMulti.py
+++ downloadImagesMulti.py
@@ -1,3 +1,18 @@
+'''
+
+구글 다운로드 추가
+# https://tiktikeuro.tistory.com/174
+# https://github.com/hellock/icrawler
+# https://github.com/YoongiKim/AutoCrawler
+
+target을 엑셀파일에서 읽어오기
+
+중단된 경우 다시 실행시키면 완료된 부분부터 시작하도록 개선
+
+'''
+
+
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 from urllib.parse import quote_plus
@@ -9,7 +24,7 @@ import time
 
 
 
-people = {'puppy': ['강다니엘', '백현', '박보검', '송중기'],
+target = {'puppy': ['강다니엘', '백현', '박보검', '송중기'],
           'cat': ['황민현', '시우민', '이종석', '강동원', '이종석', '이준기'],
           'bear': ['마동석', '조진웅', '조세호', '안재홍'],
           'dinosaur': ['윤두준', '이민기', '육성재', '공유', '김우빈'],
@@ -21,7 +36,7 @@ nameTag = 'daum'
 baseUrl = 'https://search.daum.net/search?w=img&nil_search=btn&DA=NTB&enc=utf8&q='
 
 Path("./img").mkdir(parents=True, exist_ok=True)
-for k, v in people.items():
+for k, v in target.items():
     Path("./img/" + k).mkdir(parents=True, exist_ok=True)
     for person in v:
         url = baseUrl + quote_plus(person)
@@ -58,11 +73,15 @@ for k, v in people.items():
         for i in img:
             #imgUrl = i['data-source']
             imgUrl = i['src']
-            with urlopen(imgUrl) as f:
+            try:
+                fileUrl =  urlopen(imgUrl)
+            except:
+                continue
+            else:
                 fileName = './img/' + k + '/' + person + '/' + person + '-' + nameTag + '-' + "{:0>4d}".format(n)+'.jpg'
-                with open(fileName,'wb') as h: # w - write b - binary
-                    img = f.read()
-                    h.write(img)
+                fileLocal = open(fileName,'wb')
+                img = fileUrl.read()
+                fileLocal.write(img)
             n += 1
             print('download files : ' + fileName, end='\033[K\r')
 
@@ -74,7 +93,7 @@ nameTag = 'naver'
 baseUrl = 'https://search.naver.com/search.naver?where=image&sm=tab_jum&query='
 
 Path("./img").mkdir(parents=True, exist_ok=True)
-for k, v in people.items():
+for k, v in target.items():
     Path("./img/" + k).mkdir(parents=True, exist_ok=True)
     for person in v:
         url = baseUrl + quote_plus(person)
@@ -111,21 +130,19 @@ for k, v in people.items():
         for i in img:
             #imgUrl = i['data-source']
             imgUrl = i['src']
-            with urlopen(imgUrl) as f:
+            try:
+                fileUrl =  urlopen(imgUrl)
+            except:
+                continue
+            else:
                 fileName = './img/' + k + '/' + person + '/' + person + '-' + nameTag + '-' + "{:0>4d}".format(n)+'.jpg'
-                with open(fileName,'wb') as h: # w - write b - binary
-                    img = f.read()
-                    h.write(img)
+                fileLocal = open(fileName,'wb')
+                img = fileUrl.read()
+                fileLocal.write(img)
             n += 1
             print('download files : ' + fileName, end='\033[K\r')
 
 print('\n' + nameTag + ' 다운로드 완료')
 
 
-
-# https://tiktikeuro.tistory.com/174
-
-# https://github.com/hellock/icrawler
-
-# https://github.com/YoongiKim/AutoCrawler
 
